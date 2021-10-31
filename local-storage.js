cbf2f0483d5c5013f1b24f7c7ab285a428b8ebe9
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-10-31 10:18:59 +0100

    list correctly being loaded and stored on initialization and creation of new items. basic li items being displayed on screen

--- local-storage.js
+++ local-storage.js
@@ -2,23 +2,27 @@ const LIST = [];
 const STORED_ARRAY = [JSON.parse(localStorage.getItem("todo-list"))];
 const INPUT = document.getElementById("input-text");
 const FORM = document.getElementById("form");
+const TODO_LIST = document.getElementById("todo-list");
+// const LI = document.createElement('li');
+// const LI = document.createElement('li')
 FORM.addEventListener("submit", (e) => {
   e.preventDefault();
   create();
 });
 
 // Loading the list from previous session by accessing all items of array nested in STORED_ARRAY
-const loadList = () => {
+const load = () => {
   STORED_ARRAY.forEach((item) => {
     if (item) {
-      item.forEach((i) => LIST.unshift(i));
+      item.forEach((i) => LIST.push(i));
     }
   });
 };
 
+// Creating items and giving them properties. Calling load function if no previous values
 const create = () => {
   if (!LIST.length && STORED_ARRAY) {
-    loadList();
+    load();
   }
   const item = {
     text: `${INPUT.value}`,
@@ -26,20 +30,25 @@ const create = () => {
   };
   store(item);
   INPUT.value = "";
+  read();
 };
 
 // Saving the value in local array + local browser storage
 const store = (item) => {
   LIST.unshift(item);
-  console.log(LIST);
   localStorage.setItem(`todo-list`, JSON.stringify(LIST));
 };
 
-// localStorage.setItem('item', JSON.stringify(ITEM));
-// const test = localStorage.getItem('item')
-// console.log(test)
-// const p = document.createElement('p');
-// document.body.appendChild(p);
-// p.innerHTML = test
-// const parse = JSON.parse(test)
-// console.log(parse)
+const read = () => {
+  TODO_LIST.innerHTML = "";
+  // Function to iterate over list and create list items
+  LIST.forEach((item) => {
+    const li = document.createElement("li");
+    li.innerHTML = item.text;
+    TODO_LIST.appendChild(li);
+  });
+};
+
+// Loading previous session and reading list on initialization
+load();
+read();
