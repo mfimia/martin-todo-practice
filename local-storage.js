ca19f674a74bf9608a19ffb86ce3b9670710376e
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-10-31 11:11:02 +0100

    added delete button to all items. cleaned load function by adding it some logic and triggering it always when reading screen

--- local-storage.js
+++ local-storage.js
@@ -1,10 +1,8 @@
-const LIST = [];
+let LIST = [];
 const STORED_ARRAY = [JSON.parse(localStorage.getItem("todo-list"))];
 const INPUT = document.getElementById("input-text");
 const FORM = document.getElementById("form");
 const TODO_LIST = document.getElementById("todo-list");
-// const LI = document.createElement('li');
-// const LI = document.createElement('li')
 FORM.addEventListener("submit", (e) => {
   e.preventDefault();
   create();
@@ -12,25 +10,28 @@ FORM.addEventListener("submit", (e) => {
 
 // Loading the list from previous session by accessing all items of array nested in STORED_ARRAY
 const load = () => {
-  STORED_ARRAY.forEach((item) => {
-    if (item) {
-      item.forEach((i) => LIST.push(i));
-    }
-  });
+  if (!LIST.length && STORED_ARRAY) {
+    STORED_ARRAY.forEach((item) => {
+      if (item) {
+        item.forEach((i) => LIST.push(i));
+      }
+    });
+  }
 };
 
 // Creating items and giving them properties. Calling load function if no previous values
 const create = () => {
-  if (!LIST.length && STORED_ARRAY) {
-    load();
+  if (INPUT.value) {
+    const item = {
+      text: `${INPUT.value}`,
+      id: Math.floor(Math.random() * 1000000000),
+    };
+    store(item);
+    INPUT.value = "";
+    read();
+  } else {
+    alert("Please write something!");
   }
-  const item = {
-    text: `${INPUT.value}`,
-    id: Math.floor(Math.random() * 1000000000),
-  };
-  store(item);
-  INPUT.value = "";
-  read();
 };
 
 // Saving the value in local array + local browser storage
@@ -39,16 +40,33 @@ const store = (item) => {
   localStorage.setItem(`todo-list`, JSON.stringify(LIST));
 };
 
+// Displaying content on the screen. Making sure it is loaded from previous session first
 const read = () => {
+  load();
   TODO_LIST.innerHTML = "";
   // Function to iterate over list and create list items
   LIST.forEach((item) => {
     const li = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    deleteButton.setAttribute("id", `id-${item.id}`);
+    deleteButton.addEventListener("click", () => {
+      remove(item.id);
+    });
     li.innerHTML = item.text;
+    li.appendChild(deleteButton);
     TODO_LIST.appendChild(li);
   });
 };
 
+const remove = (id) => {
+  // Filtering element out of local list
+  LIST = LIST.filter((item) => {
+    return item.id != id;
+  });
+  localStorage.setItem(`todo-list`, JSON.stringify(LIST));
+  read();
+};
+
 // Loading previous session and reading list on initialization
-load();
 read();
